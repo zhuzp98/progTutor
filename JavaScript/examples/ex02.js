d09aeacae123e3202d1e7c021afdccb324cfc0b4
unknown <>
2016-09-07 19:50:28 -0400

    add some variables

--- JavaScript/examples/ex02.js
+++ JavaScript/examples/ex02.js
@@ -5,3 +5,13 @@ var number3;
 console.log(number1 + number2);
 number3 = number1 * 2 + number2;
 console.log(number3);
+
+var number4 = 7;
+var number5 = 8;
+var sum;
+
+console.log(number4-number5);
+console.log(number4*number5);
+console.log(number5/number4);
+sum = number4+number5;
+console.log(sum);
