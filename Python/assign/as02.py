055dbd46940b4a57c7290951df64d822e9dcd14f
Wally Yang <wally-yang@outlook.com>
2016-09-08 21:37:47 -0400

    update Python assignment 02

---
+++ Python/assign/as02.py
@@ -0,0 +1,12 @@
+##
+## There are three bank account with $1000, $2000, $10000
+## declare three variables for the accounts
+## Suppose the interets rate at the bank is 5%,
+## and suppose the accounts stored for 5 years
+## declare three variables to store the interets for each account
+## update the account variables with total amount
+## print the interets and total amount on the screen
+## e.g.
+## account1:
+## 276.27
+## 1276.27
