321e5a00335c57909e28c46f8cc99d96ed35e895
Wally Yang <wally-yang@outlook.com>
2016-09-08 20:51:08 -0400

    update JavaScript second lesson

--- JavaScript/examples/ex02.js
+++ JavaScript/examples/ex02.js
@@ -1,9 +1,9 @@
-var number1 = 10;
-var number2 = 3;
+var number1 = 10.5;
+var number2 = 3.3;
 var number3;
 
 console.log(number1 + number2);
-number3 = number1 * 2 + number2;
+number3 = number1 * 2.5 + number2;
 console.log(number3);
 
 var number4 = 7;
@@ -16,5 +16,17 @@ console.log(number5/number4);
 sum = number4+number5;
 console.log(sum);
 
-number1 = 15;
-number1 = number1 + 5;
+number1 = 15.5;
+number1 = number1 + 5.1;
+number1 += 5.1;
+number1 = number1 + 1;
+number1 += 1;
+number1++;
+
+number1 = 23;
+number2 = 7;
+var remainder = number1 % number2;
+console.log(remainder);
+
+var bigNumber = 2345;
+var smallDigit = 2345 % 100;
