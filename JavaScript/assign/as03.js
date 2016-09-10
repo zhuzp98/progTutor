fa5db18cce404fc71da3951e418572cbc20c19f3
wally yang <wallyyang28@gmail.com>
2016-09-10 12:33:15 +0000

    update JavaScript assign 02

---
+++ JavaScript/assign/as03.js
@@ -0,0 +1,7 @@
+/**
+   define three variables to store your first name, last name,
+   and email address
+   print the following format on the screen:
+
+   firstname lastname, email@example.com
+*/
