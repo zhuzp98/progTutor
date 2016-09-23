404c3b4ef7b5f44df0d659630c894b6135c8f75c
wally yang <wallyyang28@gmail.com>
2016-09-23 21:26:36 +0000

    add JavaScript ex04_04

---
+++ JavaScript/section04/examples/ex04_04.js
@@ -0,0 +1,13 @@
+/**
+   This program prints Hello for n times
+   And prints at least 1 time if n equals 0
+*/
+var n = 0;
+var i = 1;
+
+do {
+    console.log("Hello");
+    i++;
+} while (i <= n);
+
+console.log("Bye!");
