7b75a1d260fd45c7da62321855b48973456de9a6
wally yang <wallyyang28@gmail.com>
2016-09-25 15:29:16 +0000

    Add JavaScript Section 05

---
+++ JavaScript/section05/assignments/as05_01.js
@@ -0,0 +1,5 @@
+/**
+   Write a program to compute the cube of 1 through n
+   Store the values in a array
+   Output the results
+*/
---
+++ JavaScript/section05/examples/ex05_01.js
@@ -0,0 +1,7 @@
+var arr = [1, 3, 5, 7, 9, 10, 11];
+
+for (var i = 0; i < arr.length; i++) {
+    arr[i] = arr[i] * arr[i];
+
+    console.log("Array at " + i + " = " + arr[i]);
+}
