425ad6db16d1a0e666944965fa67fa4f68edb555
Zhipeng <jsyxzzp@outlook.com>
2016-09-27 20:28:45 -0400

    ex04_05 ex05_01 ex05_02 changed as05_01 as05_02 finished

--- JavaScript/section05/examples/ex05_01.js
+++ JavaScript/section05/examples/ex05_01.js
@@ -5,3 +5,4 @@ for (var i = 0; i < arr.length; i++) {
 
     console.log("Array at " + i + " = " + arr[i]);
 }
+console.log(arr);
--- JavaScript/section05/examples/ex05_02.js
+++ JavaScript/section05/examples/ex05_02.js
@@ -1,5 +1,5 @@
-var str = "Hello World!";
+var str = ['Hello World!', "Hello Again!"];
 
 for(var i = 0; i < str.length; i++) {
-    console.log(str[i]);
+    console.log(str[i][0]);
 }
